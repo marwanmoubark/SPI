01f338ee8e709c0108d3be092461d9110e59bcc4
marwanmoubark <mmarawan098@gmail.com>
2026-03-10 04:41:31 +0200

    Implement SPI master module with state machine

---
+++ SPI_master.v
@@ -0,0 +1,122 @@
+`timescale 1ns / 1ps
+module SPI_master #(
+    parameter SYS_FREQ = 100_000_000,
+    parameter SCLK_FREQ = 10_000_000
+)(
+    input           clk,
+    input           reset,
+
+    input           tx_start,
+    input           MISO,
+    input  [7:0]    tx_data,
+
+    output reg      MOSI,
+    output          SCLK,
+    output reg      rx_valid,
+    output reg [7:0] rx_data,
+    output reg      SS_n
+);
+//GRAY encoding
+    localparam IDLE  = 3'b000;
+    localparam START = 3'b001;
+    localparam DATA  = 3'b011;
+    localparam STOP  = 3'b010;
+
+    reg [2:0] C_state, N_state;
+
+    reg [7:0] MOSI_shift_reg;
+    reg [7:0] MISO_shift_reg;
+
+    reg [3:0] bit_counter; 
+    wire SCLK_reg;
+    assign SCLK = SCLK_reg;
+
+    SPI_CLOCK #(
+        .SYS_FREQ (SYS_FREQ),
+        .SCLK_FREQ (SCLK_FREQ)
+    ) u_SPI_CLOCK (
+        .clk   (clk),
+        .reset (reset),
+        .enable(~SS_n),
+        .SCLK  (SCLK_reg)
+    );
+
+    always @(*) begin
+        case (C_state)
+            IDLE : N_state = tx_start ? START : IDLE ;
+
+            START: N_state = (SS_n) ? IDLE : DATA;
+
+            DATA : N_state = (bit_counter == 4'd9) ? STOP : DATA;
+
+            STOP : N_state = IDLE;
+
+            default: N_state = IDLE;
+        endcase
+    end
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            C_state        <= IDLE;
+            SS_n           <= 1'b1;
+            rx_valid       <= 1'b0;
+            MOSI           <= 1'b0;
+            bit_counter    <= 4'd1;
+            MOSI_shift_reg <= 8'b0;
+            MISO_shift_reg <= 8'b0;
+        end else begin
+            C_state <= N_state;
+
+            case (C_state)
+                IDLE: begin
+                    rx_valid       <= 1'b0;
+                    if (~tx_start) begin
+                        SS_n           <= 1'b1;
+                        MOSI_shift_reg <= 8'b0; 
+                        MISO_shift_reg <= 8'b0;
+                        bit_counter    <= 4'd1;
+                        //MOSI           <= 1'b0;
+                    end
+                    else SS_n           <= 1'b0;
+                end
+
+                START: begin
+                    MOSI_shift_reg <= tx_data;  
+                    MISO_shift_reg <= 8'b0;
+
+                end
+
+                DATA: begin
+                end
+
+                STOP: begin
+                    SS_n     <= 1'b1;
+                    rx_valid <= 1'b1;
+                    rx_data <= MISO_shift_reg; 
+                end
+            endcase
+        end
+    end
+
+    always @(posedge SCLK_reg) begin
+        if (C_state == DATA) begin
+            MISO_shift_reg <= { MISO , MISO_shift_reg[7:1]};
+        end
+    end
+
+    always @(negedge SCLK_reg) begin
+        if (C_state == DATA) begin
+            bit_counter <= bit_counter + 1'b1;
+                if (bit_counter == 4'd9) begin 
+                    MOSI <= 1'b0;
+                end
+
+                else MOSI <= MOSI_shift_reg[bit_counter];
+
+        end
+    end
+    always @(negedge SS_n) begin
+        MOSI = tx_data[0];
+    end
+
+endmodule
